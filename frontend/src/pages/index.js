065793245330e36e05ce8ad76eb47181a1c3d2c5
Adrián Carmona <32549529+acarmonabelmonte@users.noreply.github.com>
2020-06-28 00:58:58 +0200

    loaded Inicio content page with graphql

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
+import useInicio from "../hooks/useInicio"
 
 const Index = () => {
+  const inicio = useInicio()
+  const { nombre, contenido, imagen } = inicio[0]
+
   return (
     <Layout>
-      <h1>Index</h1>
+      <h1>{nombre}</h1>
     </Layout>
   )
 }
