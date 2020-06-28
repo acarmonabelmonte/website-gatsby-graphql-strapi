155a3a9fa6bf0b14b674ed1a9a0adbdde7223eac
Adrián Carmona <32549529+acarmonabelmonte@users.noreply.github.com>
2020-06-28 22:01:15 +0200

    finished project

--- frontend/src/hooks/useInicio.js
+++ frontend/src/hooks/useInicio.js
@@ -1,37 +1,36 @@
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from 'gatsby'
+
 const useInicio = () => {
-  const resultado = useStaticQuery(
-    graphql`
-      query {
-        allStrapiPaginas(filter: { nombre: { eq: "Inicio" } }) {
-          nodes {
-            id
-            nombre
-            contenido
-            imagen {
-              sharp: childImageSharp {
-                fluid(
-                  maxWidth: 1500
-                  duotone: {
-                    highlight: "#222222"
-                    shadow: "#192559"
-                    opacity: 30
-                  }
-                ) {
-                  ...GatsbyImageSharpFluid_withWebp
+    
+    const resultado = useStaticQuery(
+        graphql`
+            query {
+                allStrapiPaginas( filter: { nombre: { eq: "Inicio" } } ){
+                nodes {
+                    id
+                    nombre
+                    contenido
+                    imagen {
+                        sharp: childImageSharp {
+                            fluid( maxWidth: 1200 duotone: {
+                                highlight: "#222222", shadow: "#192550", opacity: 30
+                            } ) {
+                                ...GatsbyImageSharpFluid_withWebp
+                            }
+                        }
+                    }
                 }
-              }
             }
           }
-        }
-      }
-    `
-  )
-  return resultado.allStrapiPaginas.nodes.map(inicio => ({
-    nombre: inicio.nombre,
-    contenido: inicio.contenido,
-    imagen: inicio.imagen,
-  }))
-}
+        `
+    );
 
-export default useInicio
+    return resultado.allStrapiPaginas.nodes.map( inicio => ({
+        nombre: inicio.nombre,
+        contenido: inicio.contenido,
+        imagen: inicio.imagen
+    }))
+
+}
+ 
+export default useInicio;
